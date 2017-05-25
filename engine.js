f10a3f40c7d6bb8964b2610f939533393da57148
John Kor <john.kor@gmail.com>
2017-05-25 13:35:55 -0700

    added new subproject for pricing engine 'elasticsearch'

--- engine.js
+++ engine.js
@@ -45,6 +45,9 @@ module.exports = function (options) {
       name: 'Package Concept',
       value: 'packages'
     }, {
+      name: 'Elasticsearch R&D',
+      value: 'elasticsearch'
+    }, {
       name: 'Uncategorized',
       value: ''
     }],
